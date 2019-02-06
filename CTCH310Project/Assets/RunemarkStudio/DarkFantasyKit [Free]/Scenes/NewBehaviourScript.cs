d70baf09e86f030f8f024c00a8ccb0bf4d11e958
Samuel Hillis <samuel.hillis@live.com>
2019-02-06 11:19:46 -0600

    added code for movement and looking

---
+++ CTCH310Project/Assets/RunemarkStudio/DarkFantasyKit [Free]/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f, 0f, 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, 0f, -0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.1f, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.1f, 0f, 0f);
+        }
+
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        if(v > 90)
+        {
+            transform.Rotate(90, h, 0);
+        }
+
+        else if (v < -90)
+        {
+            transform.Rotate(-90, h, 0);
+        }
+
+        else
+        {
+            transform.Rotate(-v, h, 0);
+        }
+
+    }
+    }
